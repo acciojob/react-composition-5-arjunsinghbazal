91a8419a01ca5af743484218498f35d384eb3c1e
Arjun Singh <arjunsinghbazal@gmail.com>
2023-07-12 12:45:03 +0530

    submitting code-Wed Jul 12 2023 12:45:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,10 @@ import './../styles/App.css';
 import Tab from "./Tab";
 const App = () => {
   const tabs=[{title:"tab 1",content:"Content for Tab 1"},{title:"tab 2",content:"Content for Tab 2"},{title:"tab 3",content:"Content for Tab 3"}]
+  const tabs1=[{title:"tab A",content:"Content for Tab A"},{title:"tab B",content:"Content for Tab B"},{title:"tab C",content:"Content for Tab C"}]
   return (
     <div>
-       <Tab tabs={tabs}/>
+       <Tab tabs={tabs} tabs1={tabs1}/>
     </div>
   )
 }
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-const Tab=({tabs})=>{
+const Tab=({tabs,tabs1})=>{
     const [tabClicked,settabClicked]=React.useState("Content for Tab 1");
     const setClicked=(data)=>{
        settabClicked(data)
     }
     return(
-        <div>
+       <>
+         <div>
             {tabs.map((item)=>{
                 <li onClick={setClicked(item.content)}>{item.title}</li>
             })}
             <p>{tabClicked}</p>
         </div>
+        <div>
+            {tabs1.map((item)=>{
+                <li onClick={setClicked(item.content)}>{item.title}</li>
+            })}
+            <p>{tabClicked}</p>
+        </div>
+       </>
     )
 }
 
